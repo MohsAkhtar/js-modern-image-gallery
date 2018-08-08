ac92c61da398b878944aa0743b67e647fa6e0eb8
MohsAkhtar <mohsin.akhtar0@gmail.com>
2018-08-08 15:05:23 +0100

    Added fade in animation and opacity on clicked image

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 const current = document.querySelector('#current');
-// looks in imgs class then gets all .img
+// looks in imgs class then gets all img
 const imgs = document.querySelectorAll(".imgs img");
+const opacity = 0.4;
+
+// set first image opacity
+imgs[0].style.opacity = opacity; 
+
+/* could destructure the above into this:
+const[current, imgs] = [
+    document.querySelector('#current'),
+    document.querySelectorAll(".imgs img")
+];
+*/
 
 // adds event listener so when image is clicked 
 // it is set as current
 imgs.forEach(img => 
-    img.addEventListener('click', e => (current.src = 
-    e.target.src))
+    img.addEventListener('click', imgClick)
 );
+
+function imgClick(e){
+    // Reset opacity of all images on click
+    imgs.forEach(img => (img.style.opacity = 1));
+
+    // change current image to src of clicked image
+    current.src = e.target.src;
+
+    // Add fade in class
+    current.classList.add('fade-in');
+
+    // set time out and remove the fade in class 500ms
+    setTimeout(() => current.classList.remove('fade-in'), 500)
+
+    // change the opacity to opacity var
+    e.target.style.opacity = opacity;
+}
